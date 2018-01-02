53af1bf31b9f0f648a13be96e1c962d3d8efa511
Eric Jaw <naisanza@gmail.com>
2018-01-02 14:56:43 -0500

    fix: ansible install shell script
    It needed to apt update before installing, to get the latest
    version, which it wasn't doing, because someone forgot to do
    that...

--- ansible.sh
+++ ansible.sh
@@ -5,6 +5,7 @@
 apt update && \
 apt-get install -y software-properties-common python-software-properties && \
 apt-add-repository -y 'ppa:ansible/ansible' && \
+apt update && \
 apt install -y ansible && \
 echo "Done"
 

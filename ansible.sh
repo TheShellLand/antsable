6ff7e6df08da58d30ac5256cccbfe8cd50230cc7
Eric Jaw <naisanza@gmail.com>
2018-01-06 11:24:16 +0000

    updated: execute playbook from install script
    The ansible install script has a new feature! It can now take an
    argument.
    
    What argument you say? You can give it a playbook for it to run,
    and it'll run it locally, and install to localhost!
    
    Hooray

--- ansible.sh
+++ ansible.sh
@@ -2,10 +2,15 @@
 
 # Install Ansible
 
-apt update && \
-apt-get install -y software-properties-common python-software-properties && \
-apt-add-repository -y 'ppa:ansible/ansible' && \
-apt update && \
-apt install -y ansible && \
-echo "Done"
+echo -n "Installing ansible"
+apt update >/dev/null 2>&1 && echo -n "." && \
+apt-get install -y software-properties-common python-software-properties >/dev/null 2>&1 && echo -n "." && \
+apt-add-repository -y 'ppa:ansible/ansible' >/dev/null 2>&1 && echo -n "." && \
+apt update >/dev/null 2>&1 && echo -n "." && \
+apt install -y ansible >/dev/null 2>&1 && echo -n "." && \
+echo "done"
+
+if [ ! -z "$1" ]; then
+     ansible-playbook -i localhost, -c local "$1"
+fi
 

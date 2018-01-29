f34b3957ba2b7bf4ada8f1e0b3acf2789418dafd
Eric Jaw <naisanza@gmail.com>
2018-01-29 17:59:59 +0000

    updated: ansible.sh dockerize
    Packages needed for ubuntu:16.04 docker image

--- ansible.sh
+++ ansible.sh
@@ -5,6 +5,7 @@
 echo -n "Installing ansible"
 apt update >/dev/null 2>&1 && echo -n "." && \
 apt-get install -y software-properties-common python-software-properties >/dev/null 2>&1 && echo -n "." && \
+apt-get install -y apt-transport-https sudo >/dev/null 2>&1 && echo -n "." && \
 apt-add-repository -y 'ppa:ansible/ansible' >/dev/null 2>&1 && echo -n "." && \
 apt update >/dev/null 2>&1 && echo -n "." && \
 apt install -y ansible >/dev/null 2>&1 && echo -n "." && \
@@ -13,4 +14,3 @@ echo "done"
 if [ ! -z "$1" ]; then
      ansible-playbook -i localhost, -c local "$1"
 fi
-

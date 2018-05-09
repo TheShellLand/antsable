e28d9a3c9f7bf471eb2eb6204d33a25096be97e9
Eric Jaw <naisanza@gmail.com>
2018-05-09 17:47:38 -0400

    added: 18.x ansible installer

---
+++ ansible-18.x.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install Ansible
+
+echo -n "Installing ansible"
+apt purge -y appstream >/dev/null 2>&1 && echo -n "." && \
+apt update >/dev/null 2>&1 && echo -n "." && \
+apt-get install -y apt-transport-https sudo >/dev/null 2>&1 && echo -n "." && \
+apt-add-repository -y 'ppa:ansible/ansible' >/dev/null 2>&1 && echo -n "." && \
+apt update >/dev/null 2>&1 && echo -n "." && \
+apt install -y ansible >/dev/null 2>&1 && echo -n "." && \
+echo "done"
+
+if [ ! -z "$1" ]; then
+     ansible-playbook -i localhost, -c local "$1"
+fi

574fbb486117ad10aaedb92339aa2c80b5a36dab
Eric Jaw <naisanza@gmail.com>
2018-10-03 02:12:48 -0400

    bugfix installer

--- ansible.sh
+++ ansible.sh
@@ -3,7 +3,6 @@
 # Install Ansible
 
 
-
 # Ubuntu 17.x and below
 
 grep "Ubuntu 17" /etc/issue
@@ -14,8 +13,9 @@ if [ "$?" == 0 ]; then
 			echo "Installing ansible"
 			apt purge -y appstream
 			apt update && \
-			apt-get install -y software-properties-common python-software-properties && \
-			apt-get install -y apt-transport-https sudo && \
+			apt-get install -y software-properties-common && \
+			apt-get install -y python-software-properties && \
+			apt-get install -y apt-transport-https && \
 			apt-add-repository -y 'ppa:ansible/ansible' && \
 			apt update && \
 			apt install -y ansible && \
@@ -33,7 +33,8 @@ if [ "$?" == 0 ]; then
 	if [ ! "$?" == 0 ]; then
 			echo "Installing ansible"
 			apt update && \
-			apt-get install -y apt-transport-https sudo && \
+			apt-get install -y software-properties-common && \
+			apt-get install -y apt-transport-https && \
 			apt-add-repository -y 'ppa:ansible/ansible' && \
 			apt update && \
 			apt install -y ansible && \

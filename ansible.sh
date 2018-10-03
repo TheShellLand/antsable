e8473bb4a10553236c957af981e48b11e3e7bfcd
Eric Jaw <naisanza@gmail.com>
2018-10-03 00:16:26 -0400

    Fixed bugs in ansible installer

--- ansible.sh
+++ ansible.sh
@@ -7,18 +7,18 @@
 # Ubuntu 17.x and below
 
 grep "Ubuntu 17" /etc/issue
-if [ ! "$?" == 0 ]; then
+if [ "$?" == 0 ]; then
 
-	which ansible 2>&1 >/dev/null
+	which ansible
 	if [ ! "$?" == 0 ]; then
-			echo -n "Installing ansible"
+			echo "Installing ansible"
 			apt purge -y appstream
-			apt update >/dev/null 2>&1 && echo -n "." && \
-			apt-get install -y software-properties-common python-software-properties >/dev/null 2>&1 && echo -n "." && \
-			apt-get install -y apt-transport-https sudo >/dev/null 2>&1 && echo -n "." && \
-			apt-add-repository -y 'ppa:ansible/ansible' >/dev/null 2>&1 && echo -n "." && \
-			apt update >/dev/null 2>&1 && echo -n "." && \
-			apt install -y ansible >/dev/null 2>&1 && echo -n "." && \
+			apt update && \
+			apt-get install -y software-properties-common python-software-properties && \
+			apt-get install -y apt-transport-https sudo && \
+			apt-add-repository -y 'ppa:ansible/ansible' && \
+			apt update && \
+			apt install -y ansible && \
 			echo "done"
 	fi
 fi
@@ -27,16 +27,16 @@ fi
 # Ubuntu 18.x and above
 
 grep "Ubuntu 18" /etc/issue
-if [ ! "$?" == 0 ]; then
+if [ "$?" == 0 ]; then
 
-	which ansible 2>&1 >/dev/null
+	which ansible
 	if [ ! "$?" == 0 ]; then
-			echo -n "Installing ansible"
-			apt update >/dev/null 2>&1 && echo -n "." && \
-			apt-get install -y apt-transport-https sudo >/dev/null 2>&1 && echo -n "." && \
-			apt-add-repository -y 'ppa:ansible/ansible' >/dev/null 2>&1 && echo -n "." && \
-			apt update >/dev/null 2>&1 && echo -n "." && \
-			apt install -y ansible >/dev/null 2>&1 && echo -n "." && \
+			echo "Installing ansible"
+			apt update && \
+			apt-get install -y apt-transport-https sudo && \
+			apt-add-repository -y 'ppa:ansible/ansible' && \
+			apt update && \
+			apt install -y ansible && \
 			echo "done"
 	fi
 fi

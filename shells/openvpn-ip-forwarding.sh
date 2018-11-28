4c88d12a5c81fa89c7e5e51ad85e8b24b8aad26e
Eric Jaw <naisanza@gmail.com>
2018-11-28 00:22:43 +0000

    Enable ip forwarding for openvpn + fixed bug, exit 0

---
+++ shells/openvpn-ip-forwarding.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Up script enable ip forwarding
+
+echo "Enabling IPv4 forwarding"
+echo "1" > /proc/sys/net/ipv4/ip_forward
+
+echo "Enabling IPv6 forwarding"
+echo "1" > /proc/sys/net/ipv6/ip_forward
+
+exit 0
+

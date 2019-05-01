2b885e315eb18a2b2446be768fd7681372504d54
ainiml <naisanza@gmail.com>
2019-05-01 23:13:43 +0000

    dockerize devops

--- shells/devops.sh
+++ shells/devops.sh
@@ -5,26 +5,12 @@
 cd $(dirname $0)
 
 
-which apt
-if [ ! "$?" == 0 ]; then echo "apt not found. minimum requirement not met"; exit 1; fi
-
-which git
-if [ ! "$?" == 0 ]; then apt update && apt install -y git; fi
+if [ ! $(which apt) ]; then echo "apt not found. minimum requirement not met"; exit 1; fi
 
 
-git="../.git"
 antsable="../"
 playbooks="../playbooks"
 
-if [ -d "$git" ]; then
-	git reset --hard
-	git clean -xdf
-	git pull
-else
-	echo "[error] Not a git repository"
-	echo "[error] Please try cloning repository again"
-	echo "[error] git clone git@github.com:TheShellLand/antsable.git"
-fi
 
 # Put running playbooks here
 /bin/bash $antsable/ansible.sh $antsable/playbooks/ping.yml
@@ -35,11 +21,3 @@ fi
 /bin/bash $antsable/ansible.sh $antsable/playbooks/nslookup.yml
 /bin/bash $antsable/ansible.sh $antsable/playbooks/kernelmod.yml
 
-# Put additional shell commands here
-
-
-
-# This keeps the pod alive
-while true; do
-  sleep infinity
-done

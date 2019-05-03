2e22b0d404edd25146e2a79724c8e7c188e8fc69
ainiml <ericjaw@gmail.com>
2019-05-03 17:11:22 -0400

    updated devops

--- shells/devops.sh
+++ shells/devops.sh
@@ -4,13 +4,10 @@
 
 cd $(dirname $0)
 
-
 if [ ! $(which apt) ]; then echo "apt not found. minimum requirement not met"; exit 1; fi
-
 
 antsable="../"
 playbooks="../playbooks"
-
 
 # Put running playbooks here
 /bin/bash $antsable/ansible.sh $antsable/playbooks/ping.yml

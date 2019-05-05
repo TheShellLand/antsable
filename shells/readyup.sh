efb7f2a982033d3de2b9dc6fded2c33339ab95b2
ainiml <ericjaw@gmail.com>
2019-05-05 01:57:20 -0400

    added readyup docker script

---
+++ shells/readyup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# ssh jumphost
+
+cd $(dirname $0)
+
+if [ ! $(which apt) ]; then echo "*** apt not found. minimum requirement not met ***"; exit 1; fi
+
+antsable="../"
+playbooks="../playbooks"
+
+# Put running playbooks here
+/bin/bash $antsable/ansible.sh $playbooks/ssh.yml
+/bin/bash $antsable/ansible.sh $playbooks/sshfs.yml
+/bin/bash $antsable/ansible.sh $playbooks/ping.yml
+/bin/bash $antsable/ansible.sh $playbooks/ubuntu-readyup-18.x.yml
+

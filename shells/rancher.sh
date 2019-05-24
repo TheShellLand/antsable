cfaa48abc1335c701cd488c3ca6b8c15ded04e41
ainiml <naisanza@gmail.com>
2019-05-24 02:10:22 +0000

    update rancher install

--- shells/rancher.sh
+++ shells/rancher.sh
@@ -4,19 +4,13 @@
 
 cd $(dirname $0)
 
+if [ ! "which apt" ]; then echo "apt not found. minimum requirement not met"; exit 1; fi
 
-which apt
-if [ ! "$?" == 0 ]; then echo "apt not found. minimum requirement not met"; exit 1; fi
-
-which git
-if [ ! "$?" == 0 ]; then apt update && apt install -y git; fi
-
-
-git="../.git"
 antsable="../"
 playbooks="../playbooks"
 
 # Put running playbooks here
 /bin/bash $antsable/ansible.sh $antsable/playbooks/docker.yml
 /bin/bash $antsable/ansible.sh $antsable/playbooks/rancher2.yml
+/bin/bash $antsable/ansible.sh $antsable/playbooks/rancher-cli.yml
 

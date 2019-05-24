07c9914c7d455a90514afaa8d4640799908333bf
ainiml <ericjaw@gmail.com>
2019-05-24 10:05:29 -0400

    add rancher utils

---
+++ shells/rancher-utils.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# tools for running rancher
+
+cd $(dirname $0)
+
+if [ ! "which apt" ]; then echo "apt not found. minimum requirement not met"; exit 1; fi
+
+antsable="../"
+playbooks="../playbooks"
+
+# Put running playbooks here
+/bin/bash $antsable/ansible.sh $antsable/playbooks/docker.yml
+/bin/bash $antsable/ansible.sh $antsable/playbooks/rancher-cli.yml
+/bin/bash $antsable/ansible.sh $antsable/playbooks/kubectl.yml
+

75ebc0a794489a4ee9b6f2686ada84e8249d1704
ainiml <ericjaw@gmail.com>
2019-05-31 01:29:22 -0400

    giving ansible script some love and care

--- ansible.sh
+++ ansible.sh
@@ -4,38 +4,35 @@
 
 
 # Requires apt
-which apt
-if [ ! "$?" == 0 ]; then
-  echo "apt does not exist"
-  echo "minimum requirements not met"
+if [ ! "$(which apt)" ]; then
+  echo "*** apt does not exist ***"
+  echo "*** minimum requirements not met ***"
+fi
+
+
+# Ubuntu 16.x and below
+grep "Ubuntu 16" /etc/issue
+if [ "$?" == 0 ]; then
+
+  if [ ! "$(which ansible)" ]; then
+    echo "Installing ansible"
+    apt purge -y appstream
+    apt update && \
+    apt install -y software-properties-common && \
+    apt install -y python-software-properties && \
+    apt install -y apt-transport-https && \
+    apt-add-repository -y 'ppa:ansible/ansible' && \
+    apt update && \
+    apt install -y ansible
+  fi
 fi
 
 
 # Ubuntu 17.x and below
-
-grep "Ubuntu 16" /etc/issue
-if [ "$?" == 0 ]; then
-
-  which ansible
-  if [ ! "$?" == 0 ]; then
-    echo "Installing ansible"
-    apt purge -y appstream
-    apt update && \
-    apt install -y software-properties-common && \
-    apt install -y python-software-properties && \
-    apt install -y apt-transport-https && \
-    apt-add-repository -y 'ppa:ansible/ansible' && \
-    apt update && \
-    apt install -y ansible && \
-    echo "done"
-  fi
-fi
-
 grep "Ubuntu 17" /etc/issue
 if [ "$?" == 0 ]; then
 
-  which ansible
-  if [ ! "$?" == 0 ]; then
+  if [ ! "$(which ansible)" ]; then
     echo "Installing ansible"
     apt purge -y appstream
     apt update && \
@@ -44,31 +41,28 @@ if [ "$?" == 0 ]; then
     apt install -y apt-transport-https && \
     apt-add-repository -y 'ppa:ansible/ansible' && \
     apt update && \
-    apt install -y ansible && \
-    echo "done"
+    apt install -y ansible
   fi
 fi
 
 
 # Ubuntu 18.x and above
-
 grep "Ubuntu 18" /etc/issue
 if [ "$?" == 0 ]; then
 
-  which ansible
-  if [ ! "$?" == 0 ]; then
+  if [ ! "$(which ansible)" ]; then
     echo "Installing ansible"
     apt update && \
     apt install -y software-properties-common && \
     apt install -y apt-transport-https && \
     apt-add-repository -y 'ppa:ansible/ansible' && \
     apt update && \
-    apt install -y ansible && \
-    echo "done"
+    apt install -y ansible
   fi
 fi
 
 
+# Run playbook
 if [ ! -z "$1" ]; then
   ansible-playbook -v -i localhost, -c local "$1"
 fi

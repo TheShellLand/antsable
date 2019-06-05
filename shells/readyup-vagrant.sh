d1b986811276f07b7f1728ec8cce2ca2217ca6ea
ainiml <ericjaw@gmail.com>
2019-06-05 14:41:39 -0400

    readyup for vagrant boxes

---
+++ shells/readyup-vagrant.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# ssh jumphost
+
+set -xe
+cd $(dirname $0)
+
+antsable=".."
+playbooks="../playbooks"
+
+# Put running playbooks here
+/bin/bash $antsable/ansible.sh $playbooks/ssh.yml
+/bin/bash $antsable/ansible.sh $playbooks/sshfs.yml
+/bin/bash $antsable/ansible.sh $playbooks/ping.yml
+/bin/bash $antsable/ansible.sh $playbooks/nslookup.yml
+/bin/bash $antsable/ansible.sh $playbooks/ubuntu-readyup-gnome.yml
+/bin/bash $antsable/ansible.sh $playbooks/ubuntu-readyup-18.x.yml
+/bin/bash $antsable/ansible.sh $playbooks/virtualbox-guest-addons.yml
+

332630a0164d0f0357e7add1f70864f3e2c8d1b5
ainiml <ericjaw@gmail.com>
2019-06-11 01:03:53 -0400

    added elementary os

---
+++ shells/readyup-elementaryos.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# ssh jumphost
+
+set -xe
+cd $(dirname $0)
+
+antsable=".."
+playbooks="../playbooks"
+
+# Put running playbooks here
+/bin/bash $antsable/ansible.sh $playbooks/ssh.yml
+/bin/bash $antsable/ansible.sh $playbooks/sshfs.yml
+/bin/bash $antsable/ansible.sh $playbooks/ping.yml
+/bin/bash $antsable/ansible.sh $playbooks/nslookup.yml
+/bin/bash $antsable/ansible.sh $playbooks/ubuntu-readyup-elementaryos.yml
+/bin/bash $antsable/ansible.sh $playbooks/ubuntu-readyup-18.x.yml
+

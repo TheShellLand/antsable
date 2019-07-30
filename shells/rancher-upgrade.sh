3b5627a780378292ff19b69a243540d316458587
ainiml <naisanza@gmail.com>
2019-07-30 11:31:33 -0400

    fixed rancher upgrade

--- shells/rancher-upgrade.sh
+++ shells/rancher-upgrade.sh
@@ -2,11 +2,14 @@
 
 # Upgrade rancher
 
-set -ex
-
 # Usage
 if [ -z "$1" ] || [ -z "$2" ]; then
   echo "Usage: $0 OLD_VER NEW_VER"
+  echo ""
+  docker ps | grep rancher/rancher
+  echo ""
+  docker images | grep -v agent | grep rancher/rancher
+  echo ""
   exit 1
 fi
 
@@ -18,6 +21,7 @@ RANCHER_DOCKER="rancher/rancher"
 RANCHER_VER_OLD="$1"
 RANCHER_VER_NEW="$2"
 
+set -ex
 
 # Pull images
 docker pull $RANCHER_DOCKER:$RANCHER_VER_OLD
@@ -29,4 +33,3 @@ docker stop $RANCHER_SERVER
 docker create --volumes-from $RANCHER_SERVER --name $RANCHER_DATA rancher/rancher:$RANCHER_VER_OLD
 docker rename $RANCHER_SERVER $RANCHER_SERVER_OLD
 docker run -d --volumes-from $RANCHER_DATA --restart=unless-stopped -p 8080:80 -p 8443:443 --name $RANCHER_SERVER rancher/rancher:$RANCHER_VER_NEW
-

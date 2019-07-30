81a8ddd7d4a2446205fd13e155d1965d3c6c8d4f
ainiml <naisanza@gmail.com>
2019-07-30 11:37:22 -0400

    start RANCHER_SERVER in case upgrade fails

--- shells/rancher-upgrade.sh
+++ shells/rancher-upgrade.sh
@@ -30,6 +30,6 @@ docker pull $RANCHER_DOCKER:$RANCHER_VER_NEW
 # Stop server, backup volume, run new version
 # Requires version to match running version
 docker stop $RANCHER_SERVER
-docker create --volumes-from $RANCHER_SERVER --name $RANCHER_DATA rancher/rancher:$RANCHER_VER_OLD
-docker rename $RANCHER_SERVER $RANCHER_SERVER_OLD
-docker run -d --volumes-from $RANCHER_DATA --restart=unless-stopped -p 8080:80 -p 8443:443 --name $RANCHER_SERVER rancher/rancher:$RANCHER_VER_NEW
+docker create --volumes-from $RANCHER_SERVER --name $RANCHER_DATA rancher/rancher:$RANCHER_VER_OLD || docker start $RANCHER_SERVER
+docker rename $RANCHER_SERVER $RANCHER_SERVER_OLD || docker start $RANCHER_SERVER
+docker run -d --volumes-from $RANCHER_DATA --restart=unless-stopped -p 8080:80 -p 8443:443 --name $RANCHER_SERVER rancher/rancher:$RANCHER_VER_NEW || docker start $RANCHER_SERVER

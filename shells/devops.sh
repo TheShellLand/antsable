f5c6b03c0235f3019204ff649a5a30ea574b2558
_spaceb <ericjaw@gmail.com>
2019-12-09 00:11:57 -0500

    Update devops.sh

--- shells/devops.sh
+++ shells/devops.sh
@@ -11,9 +11,9 @@ playbooks="../playbooks"
 # Put running playbooks here
 /bin/bash $antsable/ansible.sh $antsable/playbooks/ping.yml
 /bin/bash $antsable/ansible.sh $antsable/playbooks/human_tools.yml
+/bin/bash $antsable/ansible.sh $antsable/playbooks/rsync.yml
 /bin/bash $antsable/ansible.sh $antsable/playbooks/wget.yml
 /bin/bash $antsable/ansible.sh $antsable/playbooks/curl.yml
 /bin/bash $antsable/ansible.sh $antsable/playbooks/iproute2.yml
 /bin/bash $antsable/ansible.sh $antsable/playbooks/nslookup.yml
 /bin/bash $antsable/ansible.sh $antsable/playbooks/kernelmod.yml
-

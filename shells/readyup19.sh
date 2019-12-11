8668c9813726082b369b719ec850a2c0f37ee4b7
ainiml <naisanza@gmail.com>
2019-12-11 03:36:39 -0500

    ubuntu 19

---
+++ shells/readyup19.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# ssh jumphost
+
+set -xe
+cd $(dirname $0)
+
+antsable=".."
+playbooks="../playbooks"
+
+# Put running playbooks here
+/bin/bash $antsable/ansible.sh $playbooks/git.yml
+/bin/bash $antsable/ansible.sh $playbooks/ssh.yml
+/bin/bash $antsable/ansible.sh $playbooks/sshfs.yml
+/bin/bash $antsable/ansible.sh $playbooks/ping.yml
+/bin/bash $antsable/ansible.sh $playbooks/nslookup.yml
+/bin/bash $antsable/ansible.sh $playbooks/ubuntu-readyup-gnome19.yml
+# /bin/bash $antsable/ansible.sh $playbooks/ubuntu-readyup-elementaryos.yml
+#/bin/bash $antsable/ansible.sh $playbooks/docker.yml
+/bin/bash $antsable/ansible.sh $playbooks/ubuntu-readyup-19.x.yml

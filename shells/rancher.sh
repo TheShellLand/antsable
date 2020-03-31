00342ece3525c5cf8832a8c9ea277845b260ec6f
ainiml <naisanza@gmail.com>
2020-03-31 12:51:10 -0400

    fixed rancher.sh

--- shells/rancher.sh
+++ shells/rancher.sh
@@ -9,10 +9,10 @@ antsable=".."
 cd $antsable
 
 # Put running playbooks here
-./antsable/ansible.sh playbooks/docker.yml
-./antsable/ansible.sh playbooks/docker-reset.yml
-./antsable/ansible.sh playbooks/rancher2-docker.yml
-./antsable/ansible.sh playbooks/rancher-cli.yml
-./antsable/ansible.sh playbooks/kubectl.yml
-./antsable/ansible.sh playbooks/iscsi.yml
-./antsable/ansible.sh playbooks/jq.yml
+./ansible.sh playbooks/docker.yml
+./ansible.sh playbooks/docker-reset.yml
+./ansible.sh playbooks/rancher2-docker.yml
+./ansible.sh playbooks/rancher-cli.yml
+./ansible.sh playbooks/kubectl.yml
+./ansible.sh playbooks/iscsi.yml
+./ansible.sh playbooks/jq.yml

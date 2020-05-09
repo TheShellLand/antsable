9bc02f7057ca40b2cf9e4d5c5799510e03f88d98
ainiml <naisanza@gmail.com>
2020-05-09 04:48:57 -0400

    cleanup rancher deploy logging

--- shells/rancher-rebuild-cluster.sh
+++ shells/rancher-rebuild-cluster.sh
@@ -5,7 +5,7 @@
 HOST=$1
 TOKEN=$2
 
-if rancher cluster; then
+if rancher cluster >/dev/null; then
   if [ "$HOST" == "" ] || [ "$TOKEN" == "" ]; then
     echo "Usage: $0 HOST TOKEN"
     echo "* first start rancher server"
@@ -14,12 +14,12 @@ if rancher cluster; then
   fi
 fi
 
-if ! which rancher || ! which kubectl ; then
+if ! which rancher >/dev/null || ! which kubectl >/dev/null; then
   echo "*** rancher-cli and kubectl are required ***"
   exit 1
 fi
 
-set -ex
+set -e
 
 # auth to rancher
 rancher login $HOST --token $TOKEN --skip-verify
@@ -37,37 +37,54 @@ echo "$CONTEXT" | rancher context switch
 rancher cluster add-node --worker -q "$CLUSTERNAME"
 
 # start
-date
+START=$(date)
 
 # wait for cluster to start
 rancher wait --timeout 999999999 "$CLUSTERNAME"
 
-# finish
-START=$(date)
+if [ -d $CLUSTERNAME ]; then
+  echo -e "\rDeploying to $CLUSTERNAME"
+else
+  echo -e "\rDeployment yamls not found, can't deploy $CLUSTERNAME"
+  exit 1
+fi
 
-# begin deploying out to cluster
-for PROJECT in $(ls "$CLUSTERNAME"); do
+{
+  # begin deploying out to cluster
+  for PROJECT in $(ls "$CLUSTERNAME"); do
 
-  # create project
-  rancher projects | grep "$PROJECT" || rancher projects create "$PROJECT"
+    echo -ne "Deploying to Project: $PROJECT\n"
 
-  # switch to the project
-  CONTEXT=$(echo 0 | rancher context switch | grep "$CLUSTERNAME" | grep "$PROJECT" | awk '{print $1}')
-  echo "$CONTEXT" | rancher context switch
+    # create project
+    rancher projects | grep "$PROJECT" >&2 || rancher projects create "$PROJECT" >&2
 
-  for NAMESPACE in $(ls "$CLUSTERNAME/$PROJECT"); do
-    # create namespaces
-    rancher namespace create "$NAMESPACE" || echo exists
+    # switch to the project
+    CONTEXT=$(echo 0 | rancher context switch | grep "$CLUSTERNAME" | grep "$PROJECT" | awk '{print $1}') >&2
+    echo "$CONTEXT" | rancher context switch >&2
 
-    # create deployments
-    for yaml in $(find "$CLUSTERNAME/$PROJECT/$NAMESPACE" -type f -name '*.yaml'); do
-      rancher kubectl create -f "$yaml" --namespace "$NAMESPACE" || continue
+    for NAMESPACE in $(ls "$CLUSTERNAME/$PROJECT"); do
+
+      echo -ne "$NAMESPACE"
+
+      # create namespaces
+      rancher namespace create "$NAMESPACE" || echo -ne
+
+      # create deployments
+      for yaml in $(find "$CLUSTERNAME/$PROJECT/$NAMESPACE" -type f -name '*.yaml'); do
+        rancher kubectl create -f "$yaml" --namespace "$NAMESPACE" || echo -ne
+
+        echo -ne "."
+
+      done
+
     done
+    echo -ne "\n"
 
   done
-done
+} 2>/dev/null
 
 # finish
+echo -ne "\n"
 echo "Start: $START"
 echo "Finish: $(date)"
 
--- shells/rancher-rebuild-deployments.sh
+++ shells/rancher-rebuild-deployments.sh
@@ -5,7 +5,7 @@
 HOST=$1
 TOKEN=$2
 
-if rancher cluster; then
+if rancher cluster >/dev/null; then
   if [ "$HOST" == "" ] || [ "$TOKEN" == "" ]; then
     echo "Usage: $0 HOST TOKEN"
     echo "* first start rancher server"
@@ -24,25 +24,39 @@ set -e
 {
   # auth to rancher
   # Default context
-  CONTEXT=$(echo 0 | rancher login $HOST --token $TOKEN --skip-verify | grep "$CLUSTERNAME" | grep Default | awk '{print $1}' || echo -ne)
-  echo $CONTEXT | rancher login $HOST --token $TOKEN --skip-verify
+  echo -ne "\rLogging into $HOST "
+  CONTEXT=$(echo 0 | rancher login $HOST --token $TOKEN --skip-verify 2>&1 | grep "$CLUSTERNAME" | grep Default | awk '{print $1}')
+  echo $CONTEXT | rancher login $HOST --token $TOKEN --skip-verify >&2
+  echo -e "OK"
 } 2>/dev/null
 
 # start
 START=$(date)
 
+if [ -d $CLUSTERNAME ]; then
+  echo -e "\rDeploying to $CLUSTERNAME"
+else
+  echo -e "\rDeployment yamls not found, can't deploy $CLUSTERNAME"
+  exit 1
+fi
+
 {
   # begin deploying out to cluster
   for PROJECT in $(ls "$CLUSTERNAME"); do
 
+    echo -ne "Deploying to Project: $PROJECT\n"
+
     # create project
-    rancher projects | grep "$PROJECT" || rancher projects create "$PROJECT" >&2
+    rancher projects | grep "$PROJECT" >&2 || rancher projects create "$PROJECT" >&2
 
     # switch to the project
     CONTEXT=$(echo 0 | rancher context switch | grep "$CLUSTERNAME" | grep "$PROJECT" | awk '{print $1}') >&2
     echo "$CONTEXT" | rancher context switch >&2
 
     for NAMESPACE in $(ls "$CLUSTERNAME/$PROJECT"); do
+
+      echo -ne "$NAMESPACE"
+
       # create namespaces
       rancher namespace create "$NAMESPACE" || echo -ne
 
@@ -50,15 +64,17 @@ START=$(date)
       for yaml in $(find "$CLUSTERNAME/$PROJECT/$NAMESPACE" -type f -name '*.yaml'); do
         rancher kubectl create -f "$yaml" --namespace "$NAMESPACE" || echo -ne
 
-        echo -ne "\r                                                                                            "
-        echo -ne "\rDeploying: $PROJECT::$NAMESPACE::$yaml"
+        echo -ne "."
 
       done
 
     done
+    echo -ne "\n"
+
   done
 } 2>/dev/null
 
 # finish
-echo -e "\nStart: $START"
+echo -ne "\n"
+echo "Start: $START"
 echo "Finish: $(date)"

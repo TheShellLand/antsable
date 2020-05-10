3235f9f4a0e84a79ce872c44e3f9c1e66b2fc604
ainiml <naisanza@gmail.com>
2020-05-10 07:35:48 -0400

    add kubectl-all-logs.sh

---
+++ shells/kubectl-all-logs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# grep all the logs
+
+kubectl get deploy --all-namespaces | tail +2 | awk '{print "kubectl -n " $1 " logs deployment/" $2 " --all-containers"}' | xargs  -L 1 -I {} bash -c {} | grep --color "$@"

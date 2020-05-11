7ec9871361f69e385529e8f1fe359fd718af385e
ainiml <naisanza@gmail.com>
2020-05-11 15:18:27 -0400

    fixed kubectl scripts

--- shells/kubectl-all-logs.sh
+++ shells/kubectl-all-logs.sh
@@ -2,4 +2,4 @@
 
 # less all the logs
 
-kubectl get deploy --all-namespaces | tail +2 | awk '{print "kubectl -n " $1 " logs deployment/" $2 " --all-containers"}' | xargs -L 1 -I {} bash -c "{} | sed '1 i\{}\n' | less"
+kubectl get deploy --all-namespaces | tail +2 | awk '{print "kubectl -n " $1 " logs deployment/" $2 " --all-containers"}' | xargs -L 1 -I {} bash -c "{}"

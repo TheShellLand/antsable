13a27f9f53c7b95fbf523e6c28cdc1d1f12eddad
ainiml <naisanza@gmail.com>
2020-05-20 02:07:28 -0400

    add elasticsearch deploy scritp

--- shells/elasticsearch.sh
+++ shells/elasticsearch.sh
@@ -4,11 +4,12 @@
 
 set -xe
 
-rm filebeat_initialized
+rm filebeat_initialized || echo -ne
 docker rm -f elasticsearch || echo -ne
 docker rm -f kibana || echo -ne
 docker run --rm -d -p 9200:9200 --name elasticsearch --hostname elasticsearch -e discovery.type=single-node docker.elastic.co/elasticsearch/elasticsearch-oss:7.6.2
 docker run --rm -d -p 5601:5601 --name kibana -e ELASTICSEARCH_HOSTS=http://172.17.0.2:9200 docker.elastic.co/kibana/kibana-oss:7.6.2
-docker ps
+docker ps | grep elasticsearch
+docker ps | grep kibana
 
 #docker logs -f kibana

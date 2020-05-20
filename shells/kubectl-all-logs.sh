8ddd23e25f9499a8c47db5171597d552fa6d124f
ainiml <naisanza@gmail.com>
2020-05-20 02:07:56 -0400

    update kubectl-all-logs.sh

--- shells/kubectl-all-logs.sh
+++ shells/kubectl-all-logs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# less all the logs
+# cat all the logs
 
 kubectl get deploy --all-namespaces | tail +2 | awk '{print "kubectl -n " $1 " logs deployment/" $2 " --all-containers"}' | xargs -L 1 -I {} bash -c "{}"

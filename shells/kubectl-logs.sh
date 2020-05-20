8dfe11a78e21b07bfc56e11926993989f98721ad
ainiml <naisanza@gmail.com>
2020-05-20 02:08:10 -0400

    update kubectl-logs.sh

--- shells/kubectl-logs.sh
+++ shells/kubectl-logs.sh
@@ -3,7 +3,7 @@
 # grep all the logs
 
 if [ "$1" == "" ]; then
-  echo -ne "Usage: kubectl-logs.sh namespace deployemnt\n\n"
+  echo -ne "Usage: kubectl-logs.sh namespace deployment\n\n"
   kubectl get deploy --all-namespaces
 else
   kubectl -n $1 logs deployment/$2 --all-containers | less

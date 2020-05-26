26f5e37e88eda43456261b741ddb899f73fbcb66
ainiml <naisanza@gmail.com>
2020-05-26 11:47:07 -0400

    supports json and jq filters in kubectl-get-secrets.sh

--- shells/kubectl-get-secrets.sh
+++ shells/kubectl-get-secrets.sh
@@ -5,5 +5,11 @@
 if [ -z "$*" ]; then
   kubectl get secrets --all-namespaces
 else
-  kubectl describe secrets --namespace "$@"
+  if [ -z "$4" ]; then
+    set -x
+    kubectl get secrets --namespace "$1" "$2" -o json | jq "$3"
+  else
+    set -x
+    kubectl get secrets --namespace "$1" "$2" -o json | jq "$4" "$3"
+  fi
 fi

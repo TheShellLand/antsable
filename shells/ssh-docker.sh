a39591026927fc0456f8cccac8f5c5d733ad6ee8
ainiml <ericjaw@gmail.com>
2020-08-23 16:15:44 -0400

    update sshd

--- shells/ssh-docker.sh
+++ shells/ssh-docker.sh
@@ -6,5 +6,5 @@ set -xe
 cd $(dirname $0) && cd ..
 
 /bin/bash ansible.sh playbooks/ssh.yml
-/bin/bash ansible.sh playbooks/sshfs.yml
-/bin/bash ansible.sh playbooks/ping.yml
+/bin/bash ansible.sh playbooks/sshfs.yml -c local -l localhost
+/bin/bash ansible.sh playbooks/ping.yml -c local -l localhost

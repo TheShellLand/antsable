b4cb740adb6d9734cc699062e1c509fd9a552091
ainiml <ericjaw@gmail.com>
2020-09-11 18:33:29 -0400

    add vmware-mount-shares.sh

---
+++ shells/vmware-mount-shares.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# vmware mount shared folder
+
+set -xe
+cd $(dirname $0) && cd ..
+
+read -p "Shared folder: " folder
+mkdir ~/$folder
+sudo mount -t fuse.vmhgfs-fuse .host:/$folder ~/$folder

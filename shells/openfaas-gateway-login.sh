a0768a4a255791ef4288ad936e93d85ade2afdbb
ainiml <ericjaw@gmail.com>
2020-09-14 03:33:53 -0400

    update openfaas-gateway-login.sh

--- shells/openfaas-gateway-login.sh
+++ shells/openfaas-gateway-login.sh
@@ -3,7 +3,6 @@
 # get openfaas gateway login
 
 set -e
-cd $(dirname $0)
 
 USER=$(kubectl -n openfaas get secret basic-auth -o json | jq '.data."basic-auth-user"' | sed 's/"//g' | base64 -d)
 PASSWORD=$(kubectl -n openfaas get secret basic-auth -o json | jq '.data."basic-auth-password"' | sed 's/"//g' | base64 -d)

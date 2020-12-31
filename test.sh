d1acbb51778e484f264f24c7595a0d5cf29cecdd
ejaw425 <eric.jaw@marriott.com>
2020-12-31 17:26:27 -0500

    add test deployments
    CYBER-594

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# deploy syslog
+
+cd $(dirname $0)
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+if [ -f sudo-password ]; then
+ ./ansible.sh -i inventory -c local --extra-vars "ansible_become_pass=$(cat sudo-password)" "$@"
+else
+  ./ansible.sh -i inventory -c local "$@"
+fi

48f3f16a66a232458100ca20839d0cbca849c1d5
ejaw425 <eric.jaw@marriott.com>
2021-04-10 12:06:27 -0400

    fix deploy
    - use env vars first

--- debug.sh
+++ debug.sh
@@ -6,10 +6,10 @@ cd $(dirname $0)
 
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-if [ -f sudo-password ]; then
- ./ansible.sh -i inventory -e "ansible_ssh_pass=$(cat sudo-password)" -e "ansible_become_pass=$(cat sudo-password)" "$@" playbooks/roles/debug.yml
-elif [ "$ANSIBLE_BECOME_PASS" != "" ]; then
+if [ "$ANSIBLE_BECOME_PASS" != "" ]; then
   ./ansible.sh -i inventory -e "ansible_ssh_user=$ANSIBLE_USER" -e "ansible_ssh_pass=$ANSIBLE_SSH_PASS" -e "ansible_become_pass=$ANSIBLE_BECOME_PASS" "$@" playbooks/roles/debug.yml
+elif [ -f sudo-password ]; then
+ ./ansible.sh -i inventory -e "ansible_ssh_pass=$(cat sudo-password)" -e "ansible_become_pass=$(cat sudo-password)" "$@" playbooks/roles/debug.yml
 else
   ./ansible.sh -i inventory "$@"
 fi

c6ccda4785e7dc5e76bce23696a21567374bc1cf
ejaw425 <eric.jaw@marriott.com>
2021-04-10 12:01:10 -0400

    update envs
    - add ansible user

--- debug.sh
+++ debug.sh
@@ -9,7 +9,7 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 if [ -f sudo-password ]; then
  ./ansible.sh -i inventory -e "ansible_ssh_pass=$(cat sudo-password)" -e "ansible_become_pass=$(cat sudo-password)" "$@" playbooks/roles/debug.yml
 elif [ "$ANSIBLE_BECOME_PASS" != "" ]; then
-  ./ansible.sh -i inventory -e "ansible_ssh_pass=$ANSIBLE_SSH_PASS" -e "ansible_become_pass=$ANSIBLE_BECOME_PASS" "$@" playbooks/roles/debug.yml
+  ./ansible.sh -i inventory -e "ansible_ssh_user=$ANSIBLE_USER" -e "ansible_ssh_pass=$ANSIBLE_SSH_PASS" -e "ansible_become_pass=$ANSIBLE_BECOME_PASS" "$@" playbooks/roles/debug.yml
 else
   ./ansible.sh -i inventory "$@"
 fi
--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 if [ -f sudo-password ]; then
  ./ansible.sh -i inventory -e "ansible_ssh_pass=$(cat sudo-password)" -e "ansible_become_pass=$(cat sudo-password)" "$@"
 elif [ "$ANSIBLE_BECOME_PASS" != "" ]; then
-  ./ansible.sh -i inventory -e "ansible_ssh_pass=$ANSIBLE_SSH_PASS" -e "ansible_become_pass=$ANSIBLE_BECOME_PASS" "$@"
+  ./ansible.sh -i inventory -e "ansible_ssh_user=$ANSIBLE_USER" -e "ansible_ssh_pass=$ANSIBLE_SSH_PASS" -e "ansible_become_pass=$ANSIBLE_BECOME_PASS" "$@"
 else
   ./ansible.sh -i inventory "$@"
 fi

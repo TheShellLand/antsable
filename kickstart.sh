d7961a26725a61d882ffa88bd7a1351c1c2763e0
naisanza <naisanza@gmail.com>
2021-04-29 14:41:01 -0400

    update scripts
    - update kickstart.sh

--- kickstart.sh
+++ kickstart.sh
@@ -2,15 +2,12 @@
 
 # Ansible kickstarter
 
-set -xe
+cd $(dirname $0) && set -xe
 
-cd $(dirname $0)
 
 apt update
 apt install -y git
 
 git clone https://github.com/TheShellLand/antsable
-
 cd antsable
-
 ./ansible.sh

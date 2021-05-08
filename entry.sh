e420150770000219e94a6852b1abbe13816ba507
ejaw425 <eric.jaw@marriott.com>
2021-05-08 19:29:59 -0400

    fix entry

--- entry.sh
+++ entry.sh
@@ -7,7 +7,7 @@ if [ "$1" == "" ]; then
 else
   set -x
   /bin/bash /ansible-deploy/deploy.sh $@
-  echo "/bin/bash /ansible-deploy/deploy.sh $@ -l @$(ls *.retry)" > last-deploy.sh
+  echo "/bin/bash /ansible-deploy/deploy.sh $@ -l @$(ls *.retry)" 2>/dev/null > last-deploy.sh
   chmod +x last-deploy.sh
 fi
 

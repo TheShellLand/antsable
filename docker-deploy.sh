da807c64a04d66ce9597d50afb1a603bd0e28e29
ejaw425 <eric.jaw@marriott.com>
2021-05-08 19:02:47 -0400

    update deploy scripts

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -2,12 +2,13 @@
 
 # deploy with docker
 
-cd $(dirname $0) && set -xe
+cd $(dirname $0) && set -e
 
 # this only works on linux
 #docker run --rm -it --network=host csaa/syslog-deploy "$@"
 
 if [ -d "$HOME/vault_ssh" ]; then
+  set -x
   docker run --rm -it \
     -v "$HOME/vault_ssh":/root/vault_ssh \
     -v "$HOME/sshv":/root/sshv \
@@ -15,7 +16,9 @@ if [ -d "$HOME/vault_ssh" ]; then
     -v $SSH_AUTH_SOCK:/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent \
     csaa/syslog-deploy $@
 elif [ -f env.sh ]; then
+  set -x
   docker run --rm -it --env-file env.sh csaa/syslog-deploy $@
 else
+  set -x
   docker run --rm -it csaa/syslog-deploy $@
 fi
--- test.sh
+++ test.sh
@@ -2,11 +2,12 @@
 
 # test docker
 
-cd $(dirname $0) && set -xe
+cd $(dirname $0) && set -e
 
 ./build.sh
 
 if [ -d "$HOME/vault_ssh" ]; then
+  set -x
   docker run --rm -it \
     -v "$HOME/vault_ssh":/root/vault_ssh \
     -v "$HOME/sshv":/root/sshv \
@@ -14,7 +15,9 @@ if [ -d "$HOME/vault_ssh" ]; then
     -v $SSH_AUTH_SOCK:/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent \
     csaa/syslog-deploy $@
 elif [ -f env.sh ]; then
+  set -x
   docker run --rm -it --env-file env.sh csaa/syslog-deploy $@
 else
+  set -x
   docker run --rm -it csaa/syslog-deploy $@
 fi

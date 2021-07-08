310c351fc65a55b5c91741bdb92feb6645e9bdd2
naisanza <naisanza@gmail.com>
2021-07-08 17:18:33 -0400

    push to docker hub

--- docker/build.sh
+++ docker/build.sh
@@ -4,5 +4,6 @@
 
 cd $(dirname $0) && set -xe
 
-docker build $@ -t antsable -f Dockerfile ..
+docker build $@ -t theshellland/antsable -f Dockerfile ..
 docker images | grep antsable
+docker push theshellland/antsable:latest

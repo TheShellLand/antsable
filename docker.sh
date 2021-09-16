5f51d3f93f6de8151b355f6583d76f5faf498bab
naisanzaa <naisanza@gmail.com>
2021-09-16 14:57:20 -0400

    mount docker external dependencies

--- docker.sh
+++ docker.sh
@@ -5,7 +5,17 @@
 cd $(dirname $0) && set -e
 
 if [ -f env.sh ]; then
-  docker run --rm -it --env-file env.sh -e GIT_TAG=$(git describe --tags --abbrev=0) -v ansible:/root theshellland/antsable ${@}
+  docker run --rm -it --env-file env.sh \
+    -e GIT_TAG=$(git describe --tags --abbrev=0) \
+    -v ansible:/root \
+    -v "$(pwd)/sshconfig":/antsable/sshconfig \
+    -v "$(pwd)/inventory.yaml":/antsable/inventory.yaml \
+    theshellland/antsable ${@}
 else
-  docker run --rm -it -e GIT_TAG=$(git describe --tags --abbrev=0) -v ansible:/root theshellland/antsable ${@}
+  docker run --rm -it
+    -e GIT_TAG=$(git describe --tags --abbrev=0) \
+    -v ansible:/root \
+    -v "$(pwd)/sshconfig":/antsable/sshconfig \
+    -v "$(pwd)/inventory.yaml":/antsable/inventory.yaml \
+     theshellland/antsable ${@}
 fi

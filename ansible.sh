8fca12179de8cf1f2e05e3d6d0576de2e6ba6d94
naisanzaa <naisanza@gmail.com>
2021-09-16 14:04:20 -0400

    fix ansible.sh better arg builder

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install Ansible
+# Run Ansible
 
 cd $(dirname $0) && set -e
 
@@ -10,19 +10,25 @@ if [ -f env.sh ]; then source env.sh; fi
 if which ansible-playbook >/dev/null; then
   GIT_TAG=$(git describe --tags --abbrev=0)
 
-  if [ -f vault-secret ] && [ "$ANSIBLE_SSH_PASS" != "" ] && [ "$ANSIBLE_BECOME_PASS" != "" ]; then
-    set -x
-    ansible-playbook --vault-password-file vault-secret \
-      -i inventory.yaml \
-      -e "git_tag=$GIT_TAG" \
-      -e "ansible_become_pass=$ANSIBLE_BECOME_PASS" \
-      -e "ansible_ssh_pass=$ANSIBLE_SSH_PASS" "$@"
-  elif [ -f vault-secret ]; then
-    set -x
-    ansible-playbook --vault-password-file vault-secret \
-      -i inventory.yaml \
-      -e "git_tag=$GIT_TAG" "$@"
-  else
-    ansible-playbook -i inventory.yaml "$@"
+  ansible_eval=ansible-playbook
+
+  if [ "$ANSIBLE_VAULT" != "" ]; then
+    echo "$ANSIBLE_VAULT" > vault-secret
+    ansible_eval="${ansible_eval} --vault-password-file vault-secret"
   fi
+
+  if [ "$ANSIBLE_SSH_PASS" != "" ]; then
+      ansible_eval="${ansible_eval} -e ansible_ssh_pass=${ANSIBLE_SSH_PASS}"
+  fi
+
+  if [ "$ANSIBLE_BECOME_PASS" != "" ]; then
+      ansible_eval="${ansible_eval} -e ansible_become_pass=${ANSIBLE_BECOME_PASS}"
+  fi
+
+  ansible_eval="${ansible_eval} -i inventory.yaml ${@}"
+
+  exec $ansible_eval
+
+else
+  echo "ansible not found. please install."
 fi

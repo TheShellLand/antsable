d53c424d4e48f7018f96530387dcf34941b0bb7b
naisanzaa <naisanza@gmail.com>
2021-09-16 16:18:23 -0400

    add docker test

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,7 @@
 
 cd $(dirname $0) && set -xe
 
-docker build $@ -t theshellland/antsable -f Dockerfile ..
+docker build $@ -t antsable -f Dockerfile ..
+docker tag antsable theshellland/antsable
 docker images | grep antsable
 docker push theshellland/antsable:latest

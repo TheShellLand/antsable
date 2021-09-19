75b322af2e5fa1f3a2a5695febf8820f9d0bcbf4
naisanzaa <naisanza@gmail.com>
2021-09-19 16:23:07 -0400

    move test to docker

---
+++ docker/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# test docker
+
+cd $(dirname $0) && set -e
+
+/bin/bash docker/build.sh
+
+if [ ! -f inventory.yaml ]; then
+  cp -v inventory-example.yaml inventory.yaml
+fi
+
+if [ -f env.sh ]; then
+  docker run --rm -it --env-file env.sh \
+    -e GIT_TAG=$(git describe --tags --abbrev=0) \
+    -v antsable:/root \
+    -v "$HOME/.ssh":/root/.ssh \
+    -v "$(pwd)/ansible.cfg":/antsable/ansible.cfg \
+    -v "$(pwd)/sshconfig":/antsable/sshconfig \
+    -v "$(pwd)/inventory.yaml":/antsable/inventory.yaml \
+    antsable ${@}
+else
+  docker run --rm -it \
+    -e GIT_TAG=$(git describe --tags --abbrev=0) \
+    -v antsable:/root \
+    -v "$HOME/.ssh":/root/.ssh \
+    -v "$(pwd)/ansible.cfg":/antsable/ansible.cfg \
+    -v "$(pwd)/sshconfig":/antsable/sshconfig \
+    -v "$(pwd)/inventory.yaml":/antsable/inventory.yaml \
+     antsable ${@}
+fi

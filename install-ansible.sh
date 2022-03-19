28c03306f6c9d18201b5fa8e77e5f090e1068f62
naisanzaa <naisanza@gmail.com>
2022-03-19 02:28:57 -0700

    install ansible has better checks

--- install-ansible.sh
+++ install-ansible.sh
@@ -2,23 +2,38 @@
 
 # Install Ansible
 
-cd $(dirname $0) && set -e
+cd $(dirname $0) && set -ex
 
-# Helps automation
-export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
+# Helps non-interactive
+export DEBIAN_FRONTEND=noninteractive
+export DEBCONF_NONINTERACTIVE_SEEN=true
 #export TZ="America/New_York"
 
-# Best effort python3
-if which python3 && which curl; then
-  if [ ! -f get-pip.py ]; then curl "https://bootstrap.pypa.io/get-pip.py" -o get-pip.py; fi
-  set -x
-  python3 "get-pip.py"
-  python3 -m pip install -U -r requirements.txt
+# required
+if ! type curl; then echo "curl not found"; exit 1; fi
+if ! type python3; then echo "python3 not found"; exit 1; fi
 
-  which ansible && exit 0
+# fix centos7
+if type localedef; then
+  if localedef -c -f UTF-8 -i en_US en_US.UTF-8; then
+    export LC_ALL=en_US.UTF-8
+    curl "https://bootstrap.pypa.io/pip/3.6/get-pip.py" -o get-pip.py
+  fi
 fi
 
-if ! which ansible; then
+if [ ! -f get-pip.py ]; then curl "https://bootstrap.pypa.io/get-pip.py" -o get-pip.py; fi
+
+python3 "get-pip.py"
+python3 -m pip install -U pip
+python3 -m pip install -U virtualenv
+
+if ! python3 -m pip install -U ansible; then
+  python3 -m virtualenv ansible
+  source ansible/bin/activate
+  python3 -m pip install -U ansible
+fi
+
+if ! type ansible; then
   echo "ansible still not found. please raise an issue"
   exit 1
 fi

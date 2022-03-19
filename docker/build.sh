39e16527a052851b6be7685accd475d1d536b827
naisanzaa <naisanza@gmail.com>
2022-03-19 02:30:00 -0700

    update docker build

--- docker/build.sh
+++ docker/build.sh
@@ -4,7 +4,12 @@
 
 cd $(dirname $0) && set -xe
 
-docker build $@ -t antsable -f ../Dockerfile ..
-docker images | grep antsable
+ARGS="$@"
+DOCKER_IMAGE="ansible"
+WORK_DIR="../../"
+
+docker build $ARGS -t $DOCKER_IMAGE -f ../Dockerfile $WORK_DIR
+docker images | grep $DOCKER_IMAGE
+
 #docker tag antsable theshellland/antsable
 #docker push theshellland/antsable:latest
